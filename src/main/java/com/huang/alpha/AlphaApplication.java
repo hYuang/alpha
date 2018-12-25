75069d24520197bf1becdb6afb61f8c3ce3ee674
hYuang <familyhuangyang@hotmail.com>
2018-12-26 00:51:19 +0800

    gradle project

---
+++ src/main/java/com/huang/alpha/AlphaApplication.java
@@ -0,0 +1,15 @@
+package com.huang.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaApplication.class, args);
+		
+	}
+
+}
+
